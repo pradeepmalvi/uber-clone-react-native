f3789f0a6eec538dd203e2d7e4459ad6aa243b3b
Pradeep <p.malviyaaa@gmail.com>
2021-08-25 17:37:33 +0530

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.text}>I am a homescreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+  },
+});
